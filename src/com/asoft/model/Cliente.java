a0fdc60bd409a40fc434048680cea78ec26c9173
Anderson Castro <andersoncastrosantos@gmail.com>
2022-12-09 08:57:30 -0300

    adicionando classe cliente

--- src/com/asoft/model/Cliente.java
+++ src/com/asoft/model/Cliente.java
@@ -1,5 +1,7 @@
 package com.asoft.model;
 
+import java.util.Objects;
+
 public class Cliente {
 
 	private Long codigo;
@@ -7,6 +9,49 @@ public class Cliente {
 	private String nome;
 	
 	private String telefone;
+
+	
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(codigo, nome, telefone);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(codigo, other.codigo) && Objects.equals(nome, other.nome)
+				&& Objects.equals(telefone, other.telefone);
+	}
 	
 	
 	
